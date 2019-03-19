fe1756c6c267ac6aac4bdac3b8aaff7fc30852ec
Kelsey Oates <kelsey.oates@gmail.com>
2019-03-19 15:34:17 -0400

    update regulatory support

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -27,10 +27,13 @@ const Services = () => (
   <div className="services">
   <img src={supportImage} className="x-small"/>
     <h3>Complete Regulatory Support</h3>
-    <p>Running a lab is complicated, demanding, and ever - evolving. We do our job so that you can do yours. We’ll make sure you have all your documentation for both federal accreditation (CLIA, CAP) and state licensures (MA, NY, CA, FL, MD, PA, RI). That way, you can focus on what you do best - directing a high-performance laboratory while we handle the regulations.</p>
-    <p>We have decades of experience with independent labs that perform CLIA moderate and high complexity laboratory developed tests (LDTs) as well as those performing clinical trial tests. Our current and past clients include biotechnology, biopharmaceutical, and diagnostic testing companies.</p>
-    <p>In addition, we offer Point of Care Testing (POCT) auditing. POCT auditing is available for physician office labs and large multi-site hospital systems to ensure regulatory compliance and the highest quality of patient care. SSLC provides a comprehensive audit report with their findings and actionable recommendations to achieve continual regulatory readiness. We utilize CLIA, CAP, MA state and the Joint Commission guidelines to conduct our comprehensive audit.</p>
-    <p>Our expertise has grown with the technology in labs and we can help you specifically with your genetic testing, including Sanger and Next-Generation Sequencing.</p>
+    <p>Running a lab is complicated, demanding, and ever-evolving. We do our job so that you can do yours. We’ll make sure your lab operates in compliance with federal accreditation (CLIA), state licensures (MA, NY, CA, MD, PA, RI), as well as other accrediting agencies (CAP, The Joint Commission, A2LA:ISO 15189). That way, you can focus on what you do best - directing a high-performance laboratory while we handle the regulations.</p>
+
+<p>We have decades of experience with independent labs that perform CLIA moderate and high complexity laboratory developed tests (LDTs) as well as those performing clinical trial tests. Our current and past clients include biotechnology, biopharmaceutical, and diagnostic testing companies.</p>
+
+<p>In addition, we offer Point of Care Testing (POCT) auditing. POCT auditing is available for physician office labs and large multi-site hospital systems to ensure regulatory compliance and the highest quality of patient care. SSLC provides a comprehensive audit report with their findings and actionable recommendations to achieve continual regulatory readiness. We utilize CLIA, CAP, MA state and the Joint Commission guidelines to conduct our comprehensive audit.</p>
+
+<p>Our expertise has grown with the technology in labs and we can help you specifically with your genetic testing, including Sanger and Next-Generation Sequencing.</p>
 
   </div>
 
