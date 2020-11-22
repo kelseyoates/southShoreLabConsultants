b1a349a23277eadf296f728120c2d1129eb80045
Kelsey Oates <kelsey.oates@gmail.com>
2020-11-22 11:12:36 -0500

    back to original

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -8,30 +8,6 @@ const Intro = () => (
 
 <section id="intro">
   
-  
-  
-  <div className = "flex flex-col  bg-teal section">
-  
- 
-  
-
-<div className="xsm:w-1/1 sm:w-2/3 align-center">
-<h3 className="text-center">Let SSLC help you set up your COVID - 19 testing today.</h3>
-<hr />
-  <div className= "text-left px-8">
-  <p>SSLC can assist you through the regulatory process - to start up a COVID-19 testing laboratory or to add COVID-19 testing to your menu. We have worked successfully with universities, biotechnology companies, and independent clinical labs.</p> 
-  
-  <p>"SSLC, Joanne, Liz, and Glenn, were a pleasure to work with, knowledgeable in all parts of the process, and provided the documents and guidance necessary to achieve our certification.  They were also great teachers for me during the process.  From start to finish we were licensed and CLIA certified in approximately 8 weeks and there is no way we could have done this without the help and guidance of the SSLC team. <b>I highly recommend their services to one and all.</b>" - Northeastern University Professor Jared Auclair</p>
-  </div>
-</div>
-
-</div>
-  
-  
-  
-  
-  
-  
 <div className = "flex flex-wrap section">
 <div className="xsm:w-1/1 sm:w-1/3 ">
   <div>
