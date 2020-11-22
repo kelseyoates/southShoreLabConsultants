17748595aa32cbd975ed57a87db91c9e29b38893
Kelsey Oates <kelsey.oates@gmail.com>
2020-11-22 10:29:49 -0500

    added covid section

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -7,6 +7,28 @@ import sslcLogo30 from "../../images/sslc-30.png";
 const Intro = () => (
 
 <section id="intro">
+  
+  
+  
+  <div className = "flex flex-wrap section">
+  
+  
+
+<div className="xsm:w-1/1 sm:w-2/3 ">
+<h3 className="text-center">Let SSLC help you set up your COVID - 19 testing today.</h3>
+<hr />
+  <div className= "text-left px-8">
+  <p>SSLC can assist you through the regulatory process - to start up a COVID-19 testing laboratory or to add COVID-19 testing to your menu. We have worked successfully with universities, biotechnology companies, and independent clinical labs. Northeastern University Professor Jared Auclair stated, "SSLC, Joanne, Liz, and Glenn, were a pleasure to work with, knowledgeable in all parts of the process, and provided the documents and guidance necessary to achieve our certification.  They were also great teachers for me during the process.  From start to finish we were licensed and CLIA certified in approximately 8 weeks and there is no way we could have done this without the help and guidance of the SSLC team. <b>I highly recommend their services to one and all.</b>"</p>
+  </div>
+</div>
+
+</div>
+  
+  
+  
+  
+  
+  
 <div className = "flex flex-wrap section">
 <div className="xsm:w-1/1 sm:w-1/3 ">
   <div>
